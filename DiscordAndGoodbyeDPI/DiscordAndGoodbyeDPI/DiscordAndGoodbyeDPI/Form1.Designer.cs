cdda40c5440cb8cdb6425f8f11f5ebee0161ace4
Giray Emir Yondemli <emiryondemli@gmail.com>
2026-03-15 15:01:05 +0300

    New Features
    - Renamed the exe to DPI Link
    - Added app icon
    - Added 'Auto Run' option that will run the logic when the application is started
    - Added input field for Discord path

--- DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/Form1.Designer.cs
+++ DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DiscordAndGoodbyeDPI;
+namespace DiscordAndGoodbyeDPI;
 
 partial class Form1
 {
@@ -27,47 +27,169 @@ partial class Form1
     private System.Windows.Forms.Button btnBrowse;
     private System.Windows.Forms.Button btnRun;
     private System.Windows.Forms.Label lblPath;
+    private System.Windows.Forms.CheckBox chkAutoRun;
+    private System.Windows.Forms.TextBox txtDiscordPath;
+    private System.Windows.Forms.Button btnBrowseDiscord;
+    private System.Windows.Forms.Label lblDiscordPath;
+    private System.Windows.Forms.Button btnSettings;
+    private System.Windows.Forms.GroupBox grpSettings;
+    private System.Windows.Forms.CheckBox chkMinimizeDpiLink;
+    private System.Windows.Forms.CheckBox chkAutoClose;
 
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
     private void InitializeComponent()
     {
-        this.txtGoodbyeDPIPath = new System.Windows.Forms.TextBox();
-        this.btnBrowse = new System.Windows.Forms.Button();
-        this.btnRun = new System.Windows.Forms.Button();
-        this.lblPath = new System.Windows.Forms.Label();
-        this.SuspendLayout();
-    
+        txtGoodbyeDPIPath = new System.Windows.Forms.TextBox();
+        btnBrowse = new System.Windows.Forms.Button();
+        btnRun = new System.Windows.Forms.Button();
+        lblPath = new System.Windows.Forms.Label();
+        chkAutoRun = new System.Windows.Forms.CheckBox();
+        txtDiscordPath = new System.Windows.Forms.TextBox();
+        btnBrowseDiscord = new System.Windows.Forms.Button();
+        lblDiscordPath = new System.Windows.Forms.Label();
+        btnSettings = new System.Windows.Forms.Button();
+        grpSettings = new System.Windows.Forms.GroupBox();
+        chkMinimizeDpiLink = new System.Windows.Forms.CheckBox();
+        chkAutoClose = new System.Windows.Forms.CheckBox();
+        grpSettings.SuspendLayout();
+        SuspendLayout();
+        // 
         // txtGoodbyeDPIPath
-        this.txtGoodbyeDPIPath.Location = new System.Drawing.Point(12, 29);
-        this.txtGoodbyeDPIPath.Size = new System.Drawing.Size(300, 20);
-    
+        // 
+        txtGoodbyeDPIPath.Location = new System.Drawing.Point(12, 29);
+        txtGoodbyeDPIPath.Name = "txtGoodbyeDPIPath";
+        txtGoodbyeDPIPath.Size = new System.Drawing.Size(300, 23);
+        txtGoodbyeDPIPath.TabIndex = 5;
+        // 
         // btnBrowse
-        this.btnBrowse.Location = new System.Drawing.Point(320, 27);
-        this.btnBrowse.Size = new System.Drawing.Size(75, 23);
-        this.btnBrowse.Text = "Browse";
-        this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
-    
+        // 
+        btnBrowse.Location = new System.Drawing.Point(320, 27);
+        btnBrowse.Name = "btnBrowse";
+        btnBrowse.Size = new System.Drawing.Size(75, 23);
+        btnBrowse.TabIndex = 6;
+        btnBrowse.Text = "Browse";
+        btnBrowse.Click += btnBrowse_Click;
+        // 
         // btnRun
-        this.btnRun.Location = new System.Drawing.Point(12, 60);
-        this.btnRun.Size = new System.Drawing.Size(383, 30);
-        this.btnRun.Text = "Run GoodbyeDPI + Launch Discord";
-        this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
-    
+        // 
+        btnRun.Location = new System.Drawing.Point(12, 110);
+        btnRun.Name = "btnRun";
+        btnRun.Size = new System.Drawing.Size(383, 30);
+        btnRun.TabIndex = 10;
+        btnRun.Text = "Run GoodbyeDPI + Launch Discord";
+        btnRun.Click += btnRun_Click;
+        // 
         // lblPath
-        this.lblPath.AutoSize = true;
-        this.lblPath.Location = new System.Drawing.Point(12, 10);
-        this.lblPath.Text = "GoodbyeDPI CMD Path:";
-
-        // DiscordAndGoodbyeDPI
-        this.ClientSize = new System.Drawing.Size(410, 100);
-        this.Controls.Add(this.txtGoodbyeDPIPath);
-        this.Controls.Add(this.btnBrowse);
-        this.Controls.Add(this.btnRun);
-        this.Controls.Add(this.lblPath);
-        this.Text = "Discord + GoodbyeDPI Launcher";
-        this.FormBorderStyle = FormBorderStyle.FixedDialog;
-        this.MaximizeBox = false;
-        this.ResumeLayout(false);
-        this.PerformLayout();
+        // 
+        lblPath.AutoSize = true;
+        lblPath.Location = new System.Drawing.Point(12, 10);
+        lblPath.Name = "lblPath";
+        lblPath.Size = new System.Drawing.Size(133, 15);
+        lblPath.TabIndex = 11;
+        lblPath.Text = "GoodbyeDPI CMD Path:";
+        // 
+        // chkAutoRun
+        // 
+        chkAutoRun.AutoSize = true;
+        chkAutoRun.Location = new System.Drawing.Point(12, 146);
+        chkAutoRun.Name = "chkAutoRun";
+        chkAutoRun.Size = new System.Drawing.Size(120, 19);
+        chkAutoRun.TabIndex = 4;
+        chkAutoRun.Text = "Auto Run on Start";
+        chkAutoRun.UseVisualStyleBackColor = true;
+        chkAutoRun.CheckedChanged += chkAutoRun_CheckedChanged;
+        // 
+        // txtDiscordPath
+        // 
+        txtDiscordPath.Location = new System.Drawing.Point(12, 79);
+        txtDiscordPath.Name = "txtDiscordPath";
+        txtDiscordPath.Size = new System.Drawing.Size(300, 23);
+        txtDiscordPath.TabIndex = 7;
+        // 
+        // btnBrowseDiscord
+        // 
+        btnBrowseDiscord.Location = new System.Drawing.Point(320, 77);
+        btnBrowseDiscord.Name = "btnBrowseDiscord";
+        btnBrowseDiscord.Size = new System.Drawing.Size(75, 23);
+        btnBrowseDiscord.TabIndex = 8;
+        btnBrowseDiscord.Text = "Browse";
+        btnBrowseDiscord.Click += btnBrowseDiscord_Click;
+        // 
+        // lblDiscordPath
+        // 
+        lblDiscordPath.AutoSize = true;
+        lblDiscordPath.Location = new System.Drawing.Point(12, 60);
+        lblDiscordPath.Name = "lblDiscordPath";
+        lblDiscordPath.Size = new System.Drawing.Size(99, 15);
+        lblDiscordPath.TabIndex = 9;
+        lblDiscordPath.Text = "Discord EXE Path:";
+        // 
+        // btnSettings
+        // 
+        btnSettings.Location = new System.Drawing.Point(320, 144);
+        btnSettings.Name = "btnSettings";
+        btnSettings.Size = new System.Drawing.Size(75, 23);
+        btnSettings.TabIndex = 1;
+        btnSettings.Text = "Settings ▼";
+        btnSettings.Click += btnSettings_Click;
+        // 
+        // grpSettings
+        // 
+        grpSettings.Controls.Add(chkMinimizeDpiLink);
+        grpSettings.Controls.Add(chkAutoClose);
+        grpSettings.Location = new System.Drawing.Point(12, 175);
+        grpSettings.Name = "grpSettings";
+        grpSettings.Size = new System.Drawing.Size(383, 100);
+        grpSettings.TabIndex = 0;
+        grpSettings.TabStop = false;
+        grpSettings.Text = "Advanced Settings";
+        // 
+        // chkMinimizeDpiLink
+        // 
+        chkMinimizeDpiLink.AutoSize = true;
+        chkMinimizeDpiLink.Checked = true;
+        chkMinimizeDpiLink.CheckState = System.Windows.Forms.CheckState.Checked;
+        chkMinimizeDpiLink.Location = new System.Drawing.Point(10, 20);
+        chkMinimizeDpiLink.Name = "chkMinimizeDpiLink";
+        chkMinimizeDpiLink.Size = new System.Drawing.Size(207, 19);
+        chkMinimizeDpiLink.TabIndex = 0;
+        chkMinimizeDpiLink.Text = "Minimize DPI Link unconditionally";
+        chkMinimizeDpiLink.CheckedChanged += chkSetting_CheckedChanged;
+        // 
+        // chkAutoClose
+        // 
+        chkAutoClose.AutoSize = true;
+        chkAutoClose.Location = new System.Drawing.Point(10, 45);
+        chkAutoClose.Name = "chkAutoClose";
+        chkAutoClose.Size = new System.Drawing.Size(248, 19);
+        chkAutoClose.TabIndex = 2;
+        chkAutoClose.Text = "Auto Close DPI Link instead of minimizing";
+        chkAutoClose.CheckedChanged += chkSetting_CheckedChanged;
+        // 
+        // Form1
+        // 
+        ClientSize = new System.Drawing.Size(410, 172);
+        Controls.Add(grpSettings);
+        Controls.Add(btnSettings);
+        Controls.Add(chkAutoRun);
+        Controls.Add(txtGoodbyeDPIPath);
+        Controls.Add(btnBrowse);
+        Controls.Add(txtDiscordPath);
+        Controls.Add(btnBrowseDiscord);
+        Controls.Add(lblDiscordPath);
+        Controls.Add(btnRun);
+        Controls.Add(lblPath);
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        MaximizeBox = false;
+        Text = "DPI Link";
+        Shown += Form1_Shown;
+        grpSettings.ResumeLayout(false);
+        grpSettings.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
     }
 
 
--- DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/Form1.cs
+++ DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/DiscordAndGoodbyeDPI/Form1.cs
@@ -15,14 +15,146 @@ namespace DiscordAndGoodbyeDPI
         {
             InitializeComponent();
 
-            // Load saved path if available
+            // Attempt to load the application's icon to display on the Window itself
+            try
+            {
+                this.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            }
+            catch { /* Ignore if it fails to extract icon */ }
+
+            // Load saved config if available
+            LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
             if (File.Exists(ConfigPath))
             {
-                string savedPath = File.ReadAllText(ConfigPath).Trim();
-                if (File.Exists(savedPath))
+                string[] lines = File.ReadAllLines(ConfigPath);
+                if (lines.Length > 0)
                 {
-                    txtGoodbyeDPIPath.Text = savedPath;
+                    string savedPath = lines[0].Trim();
+                    if (File.Exists(savedPath))
+                    {
+                        txtGoodbyeDPIPath.Text = savedPath;
+                    }
                 }
+                if (lines.Length > 1)
+                {
+                    if (bool.TryParse(lines[1].Trim(), out bool autoRun))
+                    {
+                        chkAutoRun.Checked = autoRun;
+                    }
+                }
+                if (lines.Length > 2)
+                {
+                    string discordPath = lines[2].Trim();
+                    if (File.Exists(discordPath))
+                    {
+                        txtDiscordPath.Text = discordPath;
+                    }
+                }
+                if (lines.Length > 3)
+                {
+                    if (bool.TryParse(lines[3].Trim(), out bool minDpi)) chkMinimizeDpiLink.Checked = minDpi;
+                }
+                if (lines.Length > 4)
+                {
+                    if (bool.TryParse(lines[4].Trim(), out bool autoClose)) chkAutoClose.Checked = autoClose;
+                }
+            }
+            
+            TryAutoDetectPaths();
+        }
+
+        private void TryAutoDetectPaths()
+        {
+            // Auto-detect Discord
+            if (string.IsNullOrWhiteSpace(txtDiscordPath.Text))
+            {
+                string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                string possibleDiscordPath = Path.Combine(localAppData, "Discord", "Update.exe");
+                if (File.Exists(possibleDiscordPath))
+                {
+                    txtDiscordPath.Text = possibleDiscordPath;
+                }
+            }
+
+            // Auto-detect GoodbyeDPI (looking for common batch files in the current directory or a 'goodbyedpi' subfolder)
+            if (string.IsNullOrWhiteSpace(txtGoodbyeDPIPath.Text))
+            {
+                string[] possibleFiles = { 
+                    "2_any_country_dnsredir.cmd",
+                    "2_any_country.cmd", 
+                    "1_turkey_gui.cmd",
+                    "3_all_dns_only.cmd"
+                };
+
+                string startDir = Application.StartupPath;
+                
+                // Also check a common subfolder name if not in root
+                string[] dirsToCheck = { startDir, Path.Combine(startDir, "goodbyedpi"), Path.Combine(startDir, "GoodbyeDPI") };
+
+                foreach (var dir in dirsToCheck)
+                {
+                    if (!Directory.Exists(dir)) continue;
+
+                    foreach (var file in possibleFiles)
+                    {
+                        string fullPath = Path.Combine(dir, file);
+                        if (File.Exists(fullPath))
+                        {
+                            txtGoodbyeDPIPath.Text = fullPath;
+                            return; // Stop after finding the first match
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SaveConfig()
+        {
+            string[] lines = new string[]
+            {
+                txtGoodbyeDPIPath.Text,
+                chkAutoRun.Checked.ToString(),
+                txtDiscordPath.Text,
+                chkMinimizeDpiLink.Checked.ToString(),
+                chkAutoClose.Checked.ToString()
+            };
+            File.WriteAllLines(ConfigPath, lines);
+        }
+
+        private void chkSetting_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveConfig();
+        }
+        
+        // Backward compatibility for chkAutoRun which still points here in designer
+        private void chkAutoRun_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveConfig();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            if (this.ClientSize.Height <= 175)
+            {
+                this.ClientSize = new System.Drawing.Size(410, 285);
+                btnSettings.Text = "Settings \u25B2"; // Up arrow
+            }
+            else
+            {
+                this.ClientSize = new System.Drawing.Size(410, 175);
+                btnSettings.Text = "Settings \u25BC"; // Down arrow
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (chkAutoRun.Checked && File.Exists(txtGoodbyeDPIPath.Text))
+            {
+                btnRun_Click(this, EventArgs.Empty);
             }
         }
 
@@ -39,7 +171,24 @@ namespace DiscordAndGoodbyeDPI
                 txtGoodbyeDPIPath.Text = dialog.FileName;
 
                 // Save path to config
-                File.WriteAllText(ConfigPath, dialog.FileName);
+                SaveConfig();
+            }
+        }
+
+        private void btnBrowseDiscord_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Executable Files|*.exe",
+                Title = "Select Discord Executable (Update.exe or Discord.exe)"
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDiscordPath.Text = dialog.FileName;
+
+                // Save path to config
+                SaveConfig();
             }
         }
 
@@ -53,30 +202,50 @@ namespace DiscordAndGoodbyeDPI
                 return;
             }
 
+            string discordPath = txtDiscordPath.Text;
+            if (!File.Exists(discordPath))
+            {
+                MessageBox.Show("Selected Discord executable does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             // Save current path before running
-            File.WriteAllText(ConfigPath, cmdPath);
+            SaveConfig();
 
             try
             {
                 // Run GoodbyeDPI CMD as admin
-                Process.Start(new ProcessStartInfo
+                ProcessStartInfo gbStartInfo = new ProcessStartInfo
                 {
                     FileName = cmdPath,
                     UseShellExecute = true,
                     Verb = "runas"
-                });
+                };
+                
+
+                Process.Start(gbStartInfo);
 
                 // Launch Discord after a short delay
                 Timer timer = new Timer { Interval = 1500 };
                 timer.Tick += (s, args) =>
                 {
                     timer.Stop();
-                    string discordPath = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                        "Discord", "Update.exe");
-                    Process.Start(discordPath, "--processStart Discord.exe");
+                    string argsStr = discordPath.EndsWith("Update.exe", StringComparison.OrdinalIgnoreCase) 
+                        ? "--processStart Discord.exe" 
+                        : ""; 
+                    Process.Start(discordPath, argsStr);
                 };
                 timer.Start();
+
+                // Window Handling
+                if (chkAutoClose.Checked)
+                {
+                    Application.Exit();
+                }
+                else if (chkMinimizeDpiLink.Checked)
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                }
             }
             catch (Exception ex)
             {
